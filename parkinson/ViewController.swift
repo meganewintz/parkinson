987815d98b6344cbe88e686f6f8d95e752274e92
Mégane W <meganewintz@outlook.com>
2018-03-11 22:12:28 +0100

    Initial Commit

---
+++ parkinson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parkinson
+//
+//  Created by Thierry WINTZ on 11/03/2018.
+//  Copyright © 2018 Mégane WINTZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
