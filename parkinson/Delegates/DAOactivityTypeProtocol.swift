de7101dcfa97dbca56241abedc4e41f2c5c12c2d
Florent BERLAND <florent.berland@florents-mac.local>
2018-03-20 18:21:37 +0100

    add : activity type dao

---
+++ parkinson/Delegates/DAOactivityTypeProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  DAOactivityTypeProtocol.swift
+//  parkinson
+//
+//  Created by Florent BERLAND on 20/03/2018.
+//  Copyright © 2018 Mégane WINTZ. All rights reserved.
+//
+
+import Foundation
+
+protocol DAOactivityTypeProtocol : class {
+    
+    func getActivityTypes() -> [ActivityType]?
+    func addActivityType(activity : ActivityType) -> Bool
+}
