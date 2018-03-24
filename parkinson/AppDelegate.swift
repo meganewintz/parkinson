8e83c93abbe851e8c5cd77cd47cc2c0cc41d4ae0
Mégane W <meganewintz@outlook.com>
2018-03-24 13:26:39 +0100

    before changing add activity page

--- parkinson/AppDelegate.swift
+++ parkinson/AppDelegate.swift
@@ -60,8 +60,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         //removeActivity()
-        //displayActivityTypes()
-        //displayActivities()
+        displayActivities()
         displayFreq()
         Factory.initiateActivities()
         return true
