57e6531c87816d1d7734c733a8db62abc95aaffa
FlorentBerland <florent.berland@laposte.net>
2018-03-26 16:32:42 +0200

    add DialogBoxHelper

---
+++ parkinson/Helpers/DialogBoxHelper.swift
@@ -0,0 +1,29 @@
+//
+//  DialogBoxHelper.swift
+//  parkinson
+//
+//  Created by Thierry WINTZ on 19/03/2018.
+//  Copyright © 2018 Mégane WINTZ. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DialogBoxHelper {
+    /// shows an alert dialog box with two message
+    ///
+    /// - Parameters:
+    ///   - title: title of dialog box seen as main message
+    ///   - msg: additional message used to describe context or additional information
+    static func alert(view: UIViewController, WithTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok ",
+                                     style: .default)
+        
+        alert.addAction(okAction)
+        view.present(alert,animated: true)
+    }
+
+}
